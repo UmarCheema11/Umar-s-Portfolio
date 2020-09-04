efc5297bd356ddf18e25bec06eb99131cf3bffae
Umar Ali Cheeama <umaralicheema9@gmail.com>
2020-09-04 19:10:27 +0500

    message

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+# Create a blog Models.
+# Tittle
+# publishing date
+# body
+# image
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:100]
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
+
+
+# Add the blog app to the settings
+
+# Create a migration
+
+# Migrate
+
+# add to the admin
+
